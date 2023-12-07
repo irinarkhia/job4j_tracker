47683e1a9c7d446f68fd4e1ed888ca8e30a141a4
Irina <irinarkhia@yandex.ru>
2023-12-07 15:13:59 +0300

    6. Правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/tracker/Checkstyle/broken.java
@@ -0,0 +1,16 @@
+package ru.job4j.tracker.Checkstyle;
+
+public class broken {
+    public static final String newValue = "";
+    private int sizeOfEmpty = 10;
+    String Name;
+    public String Surname;
+    void echo() { }
+    void Media (Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+    void method(int a) { }
+    void Broken() { }
+}
