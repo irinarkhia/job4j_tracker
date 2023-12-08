8a63f8fa64b7fee376980a3437f33b7054549d5b
Irina <irinarkhia@yandex.ru>
2023-12-08 12:35:17 +0300

    1.5. Поля объекта [#166083]

--- src/main/java/ru/job4j/tracker/oop/Cat.java
+++ src/main/java/ru/job4j/tracker/oop/Cat.java
@@ -1,13 +1,28 @@
 package ru.job4j.tracker.oop;
 
 public class Cat {
+    private String food;
+    private String name;
+
+    public void show() {
+        System.out.println(this.name + " " + this.food);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
     public String sound() {
         return "may-may";
     }
 
     public static void main(String[] args) {
-        Cat peppy = new Cat();
-        String say = peppy.sound();
-        System.out.println("Peppy says " + say);
+        Cat gav = new Cat();
+        gav.giveNick("Nick");
+        gav.eat("kotleta");
+        gav.show();
     }
 }
