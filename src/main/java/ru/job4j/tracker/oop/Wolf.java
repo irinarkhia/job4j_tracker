f3e9ee7f51cda2ae4c7e1089f1d2ded1f3a0571a
Irina <irinarkhia@yandex.ru>
2023-12-13 10:31:47 +0300

    1.6. Взаимодействие объектов [#166087]

--- src/main/java/ru/job4j/tracker/oop/Wolf.java
+++ src/main/java/ru/job4j/tracker/oop/Wolf.java
@@ -2,6 +2,7 @@ package ru.job4j.tracker.oop;
 
 public class Wolf {
         public void eat(Girl girl) { }
+
         public void tryEat(Ball ball) {
                 ball.tryRun(false);
         }
