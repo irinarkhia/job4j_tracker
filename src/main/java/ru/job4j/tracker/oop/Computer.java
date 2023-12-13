b470d2807a93d43f2046f30ef554a7b74a6b4f92
Irina <irinarkhia@yandex.ru>
2023-12-13 11:37:08 +0300

    Перегрузка конструктора [#173890]

--- src/main/java/ru/job4j/tracker/oop/Computer.java
+++ src/main/java/ru/job4j/tracker/oop/Computer.java
@@ -11,9 +11,20 @@ public class Computer {
         cpu = "Intel Core I7-10700K";
     }
 
+    public Computer(int ssd, String cpu) {
+        this.ssd = ssd;
+        this.cpu = cpu;
+    }
+
     public Computer(boolean multiMonitor, int ssd, String cpu) {
         this.multiMonitor = multiMonitor;
         this.ssd = ssd;
+        this.cpu = cpu;
+    }
+
+    public Computer(boolean multiMonitor, double ssd, String cpu) {
+        this.multiMonitor = multiMonitor;
+        this.ssd = (int) ssd;
         this.cpu = cpu;
     }
 
@@ -26,6 +37,10 @@ public class Computer {
     public static void main(String[] args) {
         Computer computer = new Computer(true, 500, "Intel Core I7-10700K");
         Computer computer1 = new Computer();
+        Computer third = new Computer(256, "AMD Ryzen 5 3600");
+        Computer forth = new Computer(true, 512.0, "AMD Ryzen 7 3700X");
+        forth.printInfo();
+        third.printInfo();
         computer.printInfo();
         computer1.printInfo();
     }
