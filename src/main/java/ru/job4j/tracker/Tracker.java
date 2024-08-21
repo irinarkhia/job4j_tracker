03649733972714426b8d1937db7526ec93a89c1a
Irina <irinarkhia@yandex.ru>
2024-08-21 11:13:35 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -12,14 +14,44 @@ public class Tracker {
     }
 
     public Item findById(int id) {
-        Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
+        Item o = null;
+        for (int i = 0; i < items.length; i++) {
+            int itemNumber = items[i].getId();
+            if (itemNumber == id) {
+                o = items[i];
                 break;
             }
         }
-        return rsl;
+        return o;
     }
+
+
+    public Item[] findAll() {
+        Item[] itemsWithoutNull = new Item[items.length];
+        int mySize = 0;
+        for (int i = 0; i < items.length; i++) {
+            Item item = items[i];
+            if (item != null) {
+                itemsWithoutNull[mySize] = item;
+                mySize++;
+            }
+        }
+        itemsWithoutNull = Arrays.copyOf(itemsWithoutNull, mySize);
+        return itemsWithoutNull;
+    }
+
+    public Item[] findByName(String key) {
+        int newSize = 0;
+        Item[] itemsWithTrueName = new Item[items.length];
+        for (int i = 0; i < items.length; i++) {
+            Item o = items[i];
+            if (o != null && o.getName().equals(key)) {
+                itemsWithTrueName[newSize] = o;
+                newSize++;
+            }
+        }
+        itemsWithTrueName = Arrays.copyOf(itemsWithTrueName, newSize);
+        return itemsWithTrueName;
+    }
+
 }
