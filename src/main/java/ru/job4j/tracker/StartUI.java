dbbb10a7513e131a36a97859c7cc6e9a2c23d863
Irina <irinarkhia@yandex.ru>
2024-08-22 13:07:29 +0300

    10. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -7,9 +7,6 @@ public class StartUI {
 
     public static void main(String[] args) {
         Item item1 = new Item();
-        LocalDateTime localDateTime = item1.getCreated();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        String currentDateTimeFormat = localDateTime.format(formatter);
-        System.out.println("Текущие дата и время после форматирования: " + currentDateTimeFormat);
+        System.out.println("Текущие дата и время после форматирования: " + item1.toString());
     }
 }
